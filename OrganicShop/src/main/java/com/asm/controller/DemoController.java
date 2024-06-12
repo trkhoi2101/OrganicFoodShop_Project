07c0e1d0eb47d5096f0c9ddb60c72d2e8aad9922
trkhoi2101 <tranhungkhoi2004@gmail.com>
2024-06-12 13:02:59 +0700

    add

--- OrganicShop/src/main/java/com/asm/controller/DemoController.java
+++ OrganicShop/src/main/java/com/asm/controller/DemoController.java
@@ -8,17 +8,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.asm.entity.Orders;
+import com.asm.entity.Products;
 import com.asm.entity.dao.OrdersDAO;
+import com.asm.entity.dao.ProductsDAO;
 
 @Controller
 @RequestMapping("/app")
 public class DemoController {
 	@Autowired
 	OrdersDAO orderDAO;
+	@Autowired
+	ProductsDAO productDAO;
+
 	
     @RequestMapping("/user/checkout")
     public String thanh_toan11(Model model) {
-    	List<Orders> items = orderDAO.findAll();
+    	List<Products> items = productDAO.findAll();
     	model.addAttribute("items",items);
         return "user/checkout";
     }
